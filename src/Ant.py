7645f30c96e968e6158281f463204aebb7e0d125
yorickdevries <yorickdevries@live.com>
2018-10-02 14:01:46 +0200

    Make Ant class

--- src/Ant.py
+++ src/Ant.py
@@ -1,1 +1,22 @@
+import random
+
+#Class that represents the ants functionality.
+from src.Route import Route
+
+
 class Ant:
+
+    # Constructor for ant taking a Maze and PathSpecification.
+    # @param maze Maze the ant will be running in.
+    # @param spec The path specification consisting of a start coordinate and an end coordinate.
+    def __init__(self, maze, path_specification):
+        self.maze = maze
+        self.start = path_specification.get_start()
+        self.end = path_specification.get_end()
+        self.current_position = self.start
+        self.rand = random
+
+    # Method that performs a single run through the maze by the ant.
+    # @return The route the ant found through the maze.
+    def find_route(self):
+        route = Route(self.start)
